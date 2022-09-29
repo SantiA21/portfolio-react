dce7ceb96701966480c6b14a92d1249f186d1f72
SantiA21 <nahuelcavs21@gmail.com>
2022-09-29 16:00:27 -0300

    Se agrega el proyecto

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Nator from "../assets/nator.png";
 import Tareas from "../assets/tareas.png";
 import Peliculas from "../assets/peliculas.png";
+import Pokedex from "../assets/pokedex.png";
 
 const Works = () => {
   return (
@@ -80,6 +81,29 @@ const Works = () => {
                   href="https://github.com/SantiA21/Task-Manager"
                   target="__blank"
                 >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Pokedex})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Pokedex con ReactJS, Tailwind
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://pokedex-reactapi.netlify.app" target="__blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/SantiA21/pokedex" target="__blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
