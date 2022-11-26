05961fff6983c6e523bf5574f068e98381de75b6
SantiA21 <nahuelcavs21@gmail.com>
2022-11-26 13:39:47 -0300

    Nuevos cambios

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -3,6 +3,7 @@ import Nator from "../assets/nator.png";
 import Tareas from "../assets/tareas.png";
 import Peliculas from "../assets/peliculas.png";
 import Pokedex from "../assets/pokedex.png";
+import Weather from "../assets/apiweather.png";
 import ReactImg from "../assets/react.png";
 import HTML from "../assets/html.png";
 import CSS from "../assets/css.png";
@@ -22,9 +23,11 @@ const Works = () => {
     <div
       name="works"
       className="w-full md:h-screen text-[white] bg-[black] pt-20 sm:pt-0"
-
     >
-      <div data-aos="fade-up" className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+      <div
+        data-aos="fade-up"
+        className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full"
+      >
         <div className="pb-1">
           <p className="text-4xl font-bold inline border-b-4 text-[white] border-[orange]">
             Proyectos
@@ -34,6 +37,34 @@ const Works = () => {
         {/* Container */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 ">
           {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Weather})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div "
+          >
+            {/* Hover Effect */}
+            <div className="opacity-0 group-hover:opacity-100 ">
+              <div className="flex flex-row">
+                <img className="w-[60px]" src={ReactImg} alt="" />
+                <img className="w-[60px]" src={HTML} alt="" />
+                <img className="w-[60px]" src={CSS} alt="" />
+              </div>
+              <div className="pt-8 text-center">
+                <a href="https://appiopenweather.netlify.app" target="__blank">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/SantiA21/openweatherapi"
+                  target="__blank"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
           <div
             style={{ backgroundImage: `url(${Pokedex})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
