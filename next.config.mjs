f0d300967e017c4856fae0e50d0369a2d79660f4
SantiA21 <nahuelcavs21@gmail.com>
2025-05-29 12:54:15 -0300

    varios cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +11,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
